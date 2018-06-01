16e00847d7a0e80d54fd9a1606a0d5ab7dc73e1b
Guilherme Capanema <guilherme@capanema.me>
2018-06-01 15:04:59 -0300

    Melhora layout da visualização individual da foto

--- resources/assets/js/components/FrontPhotoSingle.vue
+++ resources/assets/js/components/FrontPhotoSingle.vue
@@ -1,34 +1,34 @@
 <template>
     <div>
 		<button class="modal-close is-large" aria-label="close" @click="onPhotoClose()"></button>
-		<figure class="image">
+		<figure class="image single-photo">
 	        <img v-if="photo.id" :src="'/fotos/' + photo.id + '/arquivo?tamanho=grande'" width="100%"></img>
 		</figure>
 		<div class="section single-photo-data has-text-grey-lighter">
 			<div class="container">
 				<div class="columns">
 					<div class="column is-two-thirds">
-						<button class="button is-outlined is-inverted">CARTAZES</button>
+						<button class="button is-outlined is-inverted is-dark is-static">CARTAZES</button>
 						<div v-for="poster of posters" class="content has-margin-top-200">
 							<h2 class="title is-marginless has-text-white is-uppercase">{{ poster.text }}</h2>
 							<div>
 								<span v-if="poster.pivot.gender">
 									<span class="has-text-grey">Gênero</span>
-									<router-link :to="'/?genero=' + poster.pivot.gender" class="has-text-grey-lighter">
+									<router-link :to="'/?genero=' + poster.pivot.gender" class="has-text-grey-lighter has-text-weight-light">
 										{{ poster.pivot.gender === 'male' ? 'masculino' : 'feminino' }}
 									</router-link>
 								</span>
 								<span v-if="poster.type">
 									<span class="has-text-grey">Tipo</span>
-									<router-link :to="'/?tipo=' + poster.type" class="has-text-grey-lighter">
+									<router-link :to="'/?tipo=' + poster.type" class="has-text-grey-lighter has-text-weight-light">
 										{{ poster.type }}
 									</router-link>
 								</span>
 							</div>
 							<div v-if="poster.tags.length">
 								<span class="has-text-grey">Tags</span>
-								<span v-for="(tag, index) of poster.tags" class="has-text-grey-lighter">
-									<router-link :to="'/?tag=' + tag.text" class="has-text-grey-lighter">
+								<span v-for="(tag, index) of poster.tags" class="has-text-grey-lighter has-text-weight-light">
+									<router-link :to="'/?tag=' + tag.text" class="has-text-grey-lighter has-text-weight-light">
 										{{ tag.text }}
 									</router-link><span v-if="index < (poster.tags.length - 1)">, </span>
 								</span>
@@ -39,13 +39,13 @@
 						<div v-if="photo.date" class="content">
 							<h2 class="title is-marginless has-text-white is-uppercase">Data</h2>
 							<hr class="title-underline"></hr>
-							<div>{{ photo.date }}</div>
+							<div class="has-text-weight-light">{{ photo.date }}</div>
 						</div>
 						<div v-if="photo.city" class="content">
 							<h2 class="title is-marginless has-text-white is-uppercase">Cidade</h2>
 							<hr class="title-underline"></hr>
 							<div>
-								<router-link :to="'/?cidade=' + photo.city" class="has-text-grey-lighter">
+								<router-link :to="'/?cidade=' + photo.city" class="has-text-grey-lighter has-text-weight-light">
 									{{ photo.city }}
 								</router-link>
 							</div>
@@ -54,7 +54,7 @@
 							<h2 class="title is-marginless has-text-white is-uppercase">Fotógrafo</h2>
 							<hr class="title-underline"></hr>
 							<div>
-								<router-link :to="'/?fotografo=' + photo.photographer" class="has-text-grey-lighter">
+								<router-link :to="'/?fotografo=' + photo.photographer" class="has-text-grey-lighter has-text-weight-light">
 									{{ photo.photographer }}
 								</router-link>
 							</div>
@@ -62,7 +62,7 @@
 						<div v-if="photo.license" class="content">
 							<h2 class="title is-marginless has-text-white is-uppercase">Licença de Uso</h2>
 							<hr class="title-underline"></hr>
-							<div>{{ photo.license }}</div>
+							<div class="has-text-weight-light">{{ photo.license }}</div>
 						</div>
 					</div>
 				</div>
